44f2c72d26292cfcf96d93152f291eda9dadd105
Genner <lopezitzepcgenneralexander@gmail.com>
2024-09-02 15:56:08 -0600

    Pantalla tipo splash

--- composeApp/src/commonMain/kotlin/com/gnr/proyectod1/pantallaTipoSplash/PantallaTipoSplash.kt
+++ composeApp/src/commonMain/kotlin/com/gnr/proyectod1/pantallaTipoSplash/PantallaTipoSplash.kt
@@ -1,5 +1,24 @@
 package com.gnr.proyectod1.pantallaTipoSplash
 
-class PantallaTipoSplash {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import cafe.adriel.voyager.core.screen.Screen
 
+class PantallaTipoSplash : Screen {
+
+    @Composable
+    override fun Content() {
+
+        Column(
+            modifier = Modifier.fillMaxSize().background(color = Color.Black),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            //Imagen para la pantalla tipo splash
+        }
+    }
 }
