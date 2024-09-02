039c63b0f6d41d1b05c14f8dbd572a0fc2ca090a
Genner <lopezitzepcgenneralexander@gmail.com>
2024-09-02 15:31:01 -0600

    Implementación de librerias y dependencias de voyager

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,6 +43,9 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.voyager.navigator)
+            implementation(libs.voyager.transitions)
+            implementation(libs.voyager.tabNavigator)
         }
     }
 }
