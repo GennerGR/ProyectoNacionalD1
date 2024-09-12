c584d05f8d5b2304b4d9547c807bc902ab753449
Genner <lopezitzepcgenneralexander@gmail.com>
2024-09-12 13:20:00 -0600

    Falta ultima pantalla con datos de la liga seleccionada

--- composeApp/src/commonMain/kotlin/com/gnr/proyectod1/viewDetailsLeagues/ViewDetailsLeagues.kt
+++ composeApp/src/commonMain/kotlin/com/gnr/proyectod1/viewDetailsLeagues/ViewDetailsLeagues.kt
@@ -1,17 +1,12 @@
 package com.gnr.proyectod1.viewDetailsLeagues
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.BadgedBox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
