2a18c868a3268d6b36e2ef005ed42044b03b994c
Genner <lopezitzepcgenneralexander@gmail.com>
2024-09-12 08:54:17 -0600

    UI para la pantalla de visualizacion de detalles de la liga

---
+++ composeApp/src/commonMain/kotlin/com/gnr/proyectod1/viewDetailsLeagues/ViewDetailsLeagues.kt
@@ -0,0 +1,24 @@
+package com.gnr.proyectod1.viewDetailsLeagues
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+
+class ViewDetailsLeagues : Screen {
+
+    @Composable
+    override fun Content() {
+
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+}
